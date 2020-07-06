58e35f128b921b06dbab2308fd021cfa424144ed
Umberto Collodel <umberto.collodel@gmail.com>
2020-07-06 10:58:44 +0200

    started replication figure by income group

--- 1.3 calculate_rmse.R
+++ 1.3 calculate_rmse.R
@@ -27,11 +27,11 @@ names(figures_sample)
 # Export:
 
 figures_sample %>% 
-  map2(names(figures_sample),~ ggsave(paste0("../IEO_forecasts_material/output/figures/sample/",.y,".pdf"),.x))
+  walk2(names(figures_sample),~ ggsave(paste0("../IEO_forecasts_material/output/figures/sample/",.y,".pdf"),.x))
 
 
 
-# Forecast errors: -----
+# Forecast errors: (replication of Figure 7 of the previous report) -----
 
 # Calculate mean and median by year:
 
@@ -40,9 +40,9 @@ expected_fe2 <- final %>%
   map(~ .x %>% summarise(mean_fe2 = mean(fe2, na.rm = T), median_fe2 = median(fe2, na.rm = T)))
 
 
-final %>% 
+figures_fe <- final %>% 
   map(~ .x %>% mutate(fe2 = targety_first - variable2) %>% select(country_code,country, year, fe2)) %>%
-  map2(expected_fe2, ~ .x %>% merge(.y)) %>% 
+  map2(expected_fe2, ~ .x %>% merge(.y, by = c("year"))) %>% 
   map(~ .x %>% ggplot(aes(year)) +
   geom_point(aes(y = fe2), alpha = 0.1) +
   geom_line(aes(y = mean_fe2, group = 1, color = "Mean"),size = 1) +
@@ -57,6 +57,106 @@ final %>%
   ylim(-5,5)
   )
 
+
+figures_fe %>% 
+  walk2(names(figures_fe),~ ggsave(paste0("../IEO_forecasts_material/output/figures/forecast_errors/",.y,".pdf"),.x))
+
+
+
+# In the case of inflation, mean has some missing because the mean forecast error is much larger
+# than 5%.
+
+
+# Forecast errors with boxplots: (to see distributions) ----
+
+final %>% 
+  map(~ .x %>% mutate(fe2 = targety_first - variable2) %>% select(country_code,country, year, fe2)) %>%
+  map(~ .x %>% ggplot(aes(year, fe2)) +
+        geom_boxplot(outlier.size = 0) +
+        geom_hline(yintercept = 0) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=1),
+              legend.position = "bottom") +
+        labs(color = "") +
+        xlab("") +
+        ylab("") +
+        ylim(-5,5)
+  )
+
+
+# Forecast errors by type of economy ----
+
+country_group <- read_xlsx("../IEO_forecasts_material/raw_data/country_group.xlsx") %>% 
+  rename(country_code = ifscode) %>% 
+  select(country_code, adv, eme, lidc) %>% 
+  mutate(income_group = case_when(adv == 1 ~ "adv", eme == 1 ~ "eme"))
+
+expected_fe2_group <- final %>% 
+  map(~ .x %>% mutate(fe2 = targety_first - variable2)) %>% 
+  map(~ .x %>% merge(country_group, by = "country_code")) %>% 
+  map(~ .x %>% split(.x$income_group)) %>% 
+  modify_depth(2, ~ .x %>% group_by(year) %>%  summarise(mean_fe2 = mean(fe2, na.rm = T), median_fe2 = median(fe2, na.rm = T)))
+
+
+
+
+
+figures_fe_adv <- final %>%
+  map(~ .x %>% merge(country_group, by = "country_code")) %>% 
+  map(~ .x %>% mutate(fe2 = targety_first - variable2) %>%  filter(adv == 1) %>% select(country_code,country, year, fe2)) %>%
+  map2(expected_fe2_group, ~ .x %>% merge(.y[["adv"]], by = c("year"))) %>% 
+  map(~ .x %>% ggplot(aes(year)) +
+        geom_point(aes(y = fe2), alpha = 0.1) +
+        geom_line(aes(y = mean_fe2, group = 1, color = "Mean"),size = 1) +
+        geom_line(aes(y = median_fe2, group = 1, color = "Median"), size = 1) +
+        geom_hline(yintercept = 0) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=1),
+              legend.position = "bottom") +
+        labs(color = "") +
+        xlab("") +
+        ylab("") +
+        ylim(-15,15)
+  )
+
+figures_fe_eme <- final %>%
+  map(~ .x %>% merge(country_group, by = "country_code")) %>% 
+  map(~ .x %>% mutate(fe2 = targety_first - variable2) %>%  filter(eme == 1) %>% select(country_code,country, year, fe2)) %>%
+  map2(expected_fe2_group, ~ .x %>% merge(.y[["eme"]], by = c("year"))) %>% 
+  map(~ .x %>% ggplot(aes(year)) +
+        geom_point(aes(y = fe2), alpha = 0.1) +
+        geom_line(aes(y = mean_fe2, group = 1, color = "Mean"),size = 1) +
+        geom_line(aes(y = median_fe2, group = 1, color = "Median"), size = 1) +
+        geom_hline(yintercept = 0) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=1),
+              legend.position = "bottom") +
+        labs(color = "") +
+        xlab("") +
+        ylab("") +
+        ylim(-15,15)
+  )
+  
+  
+figures_fe_lidc <- final %>%
+    map(~ .x %>% merge(country_group, by = "country_code")) %>% 
+    map(~ .x %>% mutate(fe2 = targety_first - variable2) %>% select(country_code,country, year, fe2)) %>%
+    map(~ .x %>% gather("income_group",""))
+  map2(expected_fe2, ~ .x %>% merge(.y, by = c("year"))) %>% 
+    map(~ .x %>% ggplot(aes(year)) +
+          geom_point(aes(y = fe2), alpha = 0.1) +
+          geom_line(aes(y = mean_fe2, group = 1, color = "Mean"),size = 1) +
+          geom_line(aes(y = median_fe2, group = 1, color = "Median"), size = 1) +
+          geom_hline(yintercept = 0) +
+          theme_minimal() +
+          theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=1),
+                legend.position = "bottom") +
+          labs(color = "") +
+          xlab("") +
+          ylab("") +
+          ylim(-5,5)
+    )
+  
 
 
 
