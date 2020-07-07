40efdecaf9cd05d03c85e3345f9147beaccd1843
Umberto Collodel <umberto.collodel@gmail.com>
2020-07-07 11:02:22 +0200

    added footnote

--- 2.1 analysis_accuracy.R
+++ 2.1 analysis_accuracy.R
@@ -57,77 +57,18 @@ accuracy_bp_figure[["cagdp"]] %>%
   walk2(names(accuracy_bp_figure[["cagdp"]]), ~ ggsave(paste0("../IEO_forecasts_material/output/figures/short-run forecasts/accuracy/cagdp_",.y,".pdf"),.x))
 
 
+# Footnote:
 
-#######
+footnote = c("Root mean squared error of current year Spring WEO issue forecasts divided by the realized volatility of
+  the actual value over the period. Actual value is the first settled i.e. reported in the Fall WEO of the
+  following year.")
 
-
-
-
-# Calculation RMSE 
-
-a <- final %>% 
-  map(~ .x %>% select(year, targety_first, variable1) %>%  group_by(year) %>% summarise(rmse_first = hydroGOF::rmse(targety_first, variable1, na.rm = T))) 
-
-b <- final %>% 
-  map(~ .x %>% select(year, targety_last, variable1) %>%  group_by(year) %>% summarise(rmse_last = hydroGOF::rmse(targety_last, variable1, na.rm = T))) 
-
-a[[1]] %>%
-  merge(b[[1]]) %>%
-  gather("actual","rmse",rmse_first:ncol(.)) %>% 
-  ggplot(aes(year,rmse,col = actual)) +
-  geom_point() 
+cat(footnote, file = "../IEO_forecasts_material/output/figures/short-run forecasts/accuracy/footnote.tex")
 
 
-final %>% 
-  map(~ .x %>% group_by(year) %>% summarise(rmse_first = hydroGOF::rmse(targety_first, variable1, na.rm = T))) %>% 
-  map(~ .x %>% ggplot(aes(year, rmse_first, group = 1)) +
-        geom_line() +
-        theme_minimal() +
-        theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=1)) +
-        xlab("") +
-        ylab(""))
+# Figure 2: evolution of RMSE over different horizons:  -----
 
-rmse_adv <- final[["growth"]] %>% 
-  merge(country_group, by = c("country_code")) %>%
-  filter(adv ==1) %>% 
-  group_by(year) %>% 
-  summarise(rmse_2_adv = hydroGOF::rmse(targety_first, variable2, na.rm = T))
-
-
-rmse_eme <- final[["growth"]] %>% 
-  merge(country_group, by = c("country_code")) %>%
-  filter(eme ==1) %>% 
-  group_by(year) %>% 
-  summarise(rmse_2_eme = hydroGOF::rmse(targety_first, variable2, na.rm = T))
-
-
-rmse_lidc <- final[["growth"]] %>% 
-  merge(country_group, by = c("country_code")) %>%
-  filter(lidc ==1) %>% 
-  group_by(year) %>% 
-  summarise(rmse_2_lidc = hydroGOF::rmse(targety_first, variable2, na.rm = T))
-
-
-rmse_figure <- list(rmse_adv, rmse_eme, rmse_lidc) %>% 
-  reduce(merge, by=c("year")) %>% 
-  ggplot(aes(year)) +
-  geom_line(aes(y=rmse_2_adv, color = "Advanced", group = 1)) +
-  geom_line(aes(y=rmse_2_eme, color = "Emerging", group = 1)) +
-  geom_line(aes(y=rmse_2_lidc, color = "Low-income", group = 1)) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=1)) +
-  xlab("") +
-  ylab("RMSE") +
-  labs(colour = "Group")
-
-
-ggsave("../IEO_forecasts_material/output/figures/rmse/comparison_rmse.pdf", rmse_figure)
-
-
-# RMSE for different horizons: (Table 1 Timmerman) -----
-
-rmse_horizon_group <- final %>%
-  map(~ .x %>% merge(country_group,by =c("country_code"))) %>%
+rmse_horizon_group <- final_sr %>%
   map(~ .x %>% group_by(adv)) %>% 
   map(~ .x %>% summarise_at(vars(starts_with("variable")), ~ round(rmse(.,targety_first, na.rm = T),2))) %>%
   map(~ .x %>% mutate(adv = case_when(adv == 1 ~ "AE",
@@ -135,19 +76,19 @@ rmse_horizon_group <- final %>%
   map(~ .x %>% rename(`Income group` = adv)) %>% 
   .$growth  
 
-rmse_horizon_lidc <- final %>%
-  map(~ .x %>% merge(country_group,by =c("country_code")) %>% filter(lidc == 1)) %>%
+rmse_horizon_lidc <- final_sr %>%
+  map(~ .x %>% filter(lidc == 1)) %>%
   map(~ .x %>% summarise_at(vars(starts_with("variable")), ~ round(rmse(.,targety_first, na.rm = T),2))) %>%
   map(~ .x %>% mutate(`Income group` = "Low-income") %>% select(`Income group`, everything())) %>%  
   .$growth  
 
 
-rmse_horizon <- final %>%
-  map(~ .x %>% merge(country_group,by =c("country_code"))) %>%
+rmse_horizon <- final_sr %>%
   map(~ .x %>% summarise_at(vars(starts_with("variable")), ~ round(rmse(.,targety_first, na.rm = T),2))) %>%
   map(~ .x %>% mutate(`Income group` = "Full") %>% select(`Income group`, everything())) %>% 
   .$growth  
 
 list(rmse_horizon, rmse_horizon_group, rmse_horizon_lidc) %>% 
   reduce(rbind) %>%   
-  stargazer(summary = F, rownames = F)
+  stargazer(summary = F, rownames = F) %>% 
+  save(file = "../IEO_forecasts_material/output/tables/short-run forecasts/accuracy/rmse_forecast_horizon.tex")
