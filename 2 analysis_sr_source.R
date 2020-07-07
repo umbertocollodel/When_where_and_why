dde4a0810f588bf2b93267609ea6f470d004f07c
Umberto Collodel <umberto.collodel@gmail.com>
2020-07-07 11:24:51 +0200

    updated to new organization for bias script

--- 2 analysis_sr_source.R
+++ 2 analysis_sr_source.R
@@ -1,5 +1,10 @@
 # Prepare the dataset: ----
 
+country_group <- read_xlsx("../IEO_forecasts_material/raw_data/country_group.xlsx") %>% 
+  rename(country_code = ifscode) %>% 
+  select(country_code, adv, eme, lidc)
+
+
 final_sr <- final %>% 
   map(~ .x %>% select(country_code,country,year,targety_first,targety_last,matches("variable[1-4]$"))) %>% 
   map(~ .x %>% merge(country_group, by = c("country_code"))) %>%
@@ -8,3 +13,9 @@ final_sr <- final %>%
                                          TRUE ~ "2012:2018"))) %>% 
   map(~ .x %>% as.tibble())
 
+
+# Source and produce output: ----
+
+source("2.1 analysis_accuracy.R")
+
+source("2.2 analysis_bias.R")
