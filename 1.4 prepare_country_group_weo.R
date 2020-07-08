42d90ab558677ffd48eede9af8b9fb6aa4451166
Umberto Collodel <umberto.collodel@gmail.com>
2020-07-08 18:02:05 +0200

    have geographical country classification from weo database

---
+++ 1.4 prepare_country_group_weo.R
@@ -0,0 +1,24 @@
+# Prepare country group:
+
+groups <- list.files("../IEO_forecasts_material/raw_data/country_group_geography/") %>% 
+  str_remove(".xlsx")
+
+country_group_geography <- list.files("../IEO_forecasts_material/raw_data/country_group_geography/") %>% 
+  map_chr(~ paste0("~/Dropbox/IMF/IEO_forecasts_material/raw_data/country_group_geography/",.x)) %>% 
+  map(~ read_excel(.x)) %>% 
+  map(~ .x %>% rename(country_code= 1))
+
+names(country_group_geography) <- groups
+
+# Export:
+
+country_group_geography %>% 
+  bind_rows(.id = "group") %>% 
+  select(country_code, group) %>% 
+  save(file = "../IEO_forecasts_material/intermediate_data/country_group_geography_clean.RData")
+  
+  
+  
+
+
+  
