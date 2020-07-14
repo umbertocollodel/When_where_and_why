6e7856d89fe6259e7cf2228ce01c215946989f5f
Umberto Collodel <umberto.collodel@gmail.com>
2020-07-14 17:46:43 +0200

    reorganized script: cancelled previous graphs with mistake

--- 3.1 comparison_wb.R
+++ 3.1 comparison_wb.R
@@ -5,6 +5,8 @@ gep_data <- read_xlsx("../IEO_forecasts_material/raw_data/wb_gep.xlsx") %>%
   rename_at(vars(matches("variable")), funs(str_replace(.,"variable","wb"))) 
   
 
+# Comparison of median forecast error: ----
+
 final_sr %>% 
   .$gdp %>% 
   merge(gep_data, by=c("country","year")) %>% 
@@ -49,137 +51,59 @@ final_sr %>%
 ggsave("../IEO_forecasts_material/output/figures/comparison/WB/year_ahead_comparison.pdf")
 
 
-final_sr %>% 
-  .$gdp %>% 
-  merge(gep_data, by=c("country","year")) %>% 
-  mutate_at(vars(contains("wb")),funs(targety_first - .)) %>% 
-  mutate_at(vars(contains("variable")),funs(targety_first - .)) %>% 
-  arrange(wb2) %>%
-  mutate(rank = 1:nrow(.)) %>% 
-  filter(rank < 10) %>% 
-  unite("label",country:year, sep= " ") %>%
-  mutate(label = factor(label, label)) %>% 
-  ggplot(aes(label, wb2, fill = group)) +
-  geom_col() +
-  theme_minimal() +
-  coord_flip() +
-  xlab("") +
-  ylab("") +
-  theme(legend.position = "bottom") +
-  labs(fill = "Country group") +
-  ylim(0,-6)
+# Forecast errors during recessions and expansions: ----- 
 
 final_sr %>% 
   .$gdp %>% 
-  merge(gep_data, by=c("country","year")) %>% 
+  merge(gep_data, by=c("country","year")) %>%
   mutate_at(vars(contains("wb")),funs(targety_first - .)) %>% 
   mutate_at(vars(contains("variable")),funs(targety_first - .)) %>% 
-  arrange(variable2) %>%
-  mutate(rank = 1:nrow(.)) %>% 
-  filter(rank < 10) %>% 
-  unite("label",country:year, sep= " ") %>%
-  mutate(label = factor(label, label)) %>% 
-  ggplot(aes(label, variable2, fill = group)) +
-  geom_col() +
-  theme_minimal() +
-  coord_flip() +
-  xlab("") +
-  ylab("") +
-  theme(legend.position = "bottom") +
-  labs(fill = "Country group") +
-  ylim(0,-6)
+  mutate(recession = case_when(targety_first < 0 ~ 1,
+                               T ~ 0)) %>% 
+  group_by(recession) %>% 
+  summarise(wb2 = round(median(wb2, na.rm = T),2), imf2 = round(median(variable2, na.rm = T),2),
+            wb4 = round(median(wb4, na.rm = T),2),imf4 = round(median(variable4, na.rm = T),2)) %>%
+  setNames(c("Recession","Current-year (WB)","Current-year (IMF)","Year-ahead (WB)","Year-ahead (IMF)")) %>%
+  mutate(Recession = case_when(Recession == 1 ~ "Recession",
+                               T ~ "Non-recession")) %>% 
+  stargazer(rownames = F,
+            summary = F,
+            out = "../IEO_forecasts_material/output/tables/comparison/WB/recession_forecast_error.tex")
 
-# Let's focus on the tails: 
+
+
+
+# Detail of recession episodes: -----
 
 final_sr %>% 
   .$gdp %>% 
   merge(gep_data, by=c("country","year")) %>% 
-  arrange(targety_first) %>% 
-  mutate(rank = 1:nrow(.)) %>% 
-  filter(rank <= 6) %>% 
-  mutate_at(vars(contains("wb")),funs(targety_first - .)) %>% 
-  mutate_at(vars(contains("variable")),funs(targety_first - .)) %>% 
-  ggplot(aes(variable2, wb2)) +
-  geom_point(size = 4) +
-  theme_minimal() +
-  geom_abline(intercept = 0, col = "red", size = 1.5) +
-  xlim(-6,2) +
-  ylim(-6,2) +
-  xlab("WEO Forecast error") +
-  ylab("GEP Forecast error") +
-  theme(axis.text = element_text(size = 18),
-  axis.title = element_text(size = 21))
-
-ggsave("../IEO_forecasts_material/output/figures/comparison/WB/comparison_negative_current.pdf")
-
-final_sr %>% 
-  .$gdp %>% 
-  merge(gep_data, by=c("country","year")) %>% 
-  arrange(targety_first) %>% 
-  mutate(rank = 1:nrow(.)) %>% 
-  filter(rank <= 6) %>% 
-  mutate_at(vars(contains("wb")),funs(targety_first - .)) %>% 
-  mutate_at(vars(contains("variable")),funs(targety_first - .)) %>% 
-  ggplot(aes(variable4, wb4)) +
-  geom_point(size = 4) +
-  theme_minimal() +
-  geom_abline(intercept = 0, col = "red", size = 1.5) +
-  xlim(-7,1) +
-  ylim(-7,1) +
-  xlab("WEO Forecast error") +
-  ylab("GEP Forecast error") +
-  theme(axis.text = element_text(size = 18),
+  mutate(recession = case_when(targety_first < 0 ~ 1,
+                               T ~ 0)) %>% 
+  filter(recession == 1) %>%
+  select(country, year, targety_first, variable2, wb2) %>%
+  unite("label", country:year, sep = " ", remove = F) %>% 
+  gather("var","value",targety_first:wb2) %>% 
+  mutate(var = case_when(var == "targety_first" ~ "Actual",
+         var == "variable2" ~ "WEO Forecast",
+         T ~ "GEP Forecast")) %>% 
+  ggplot(aes(label, value, col = var, group = var)) +
+    geom_line(size = 2) +
+    geom_point(size = 3) +
+    theme_minimal() +
+    xlab("") +
+    ylab("") +
+    labs(col = "") +
+    theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=1)) +
+    theme(axis.text = element_text(size = 18),
         axis.title = element_text(size = 21))
 
-ggsave("../IEO_forecasts_material/output/figures/comparison/WB/comparison_negative_yearahead.pdf")
-
-footnote=c("The figure shows the forecast error in the ")
- 
-
-# Booms:
-
-final_sr %>% 
-  .$gdp %>% 
-  merge(gep_data, by=c("country","year")) %>% 
-  arrange(targety_first) %>% 
-  mutate(rank = 1:nrow(.)) %>% 
-  filter(rank > 6) %>% 
-  mutate_at(vars(contains("wb")),funs(targety_first - .)) %>% 
-  mutate_at(vars(contains("variable")),funs(targety_first - .)) %>% 
-  ggplot(aes(variable2, wb2)) +
-  geom_point(size = 4) +
-  theme_minimal() +
-  geom_abline(intercept = 0, col = "red", size = 1.5) +
-  xlim(-4,10) +
-  ylim(-4,10) +
-  xlab("WEO Forecast error") +
-  ylab("GEP Forecast error") +
-  theme(axis.text = element_text(size = 18),
-        axis.title = element_text(size = 21))
-
-ggsave("../IEO_forecasts_material/output/figures/comparison/WB/comparison_negative_current.pdf")
-
-final_sr %>% 
-  .$gdp %>% 
-  merge(gep_data, by=c("country","year")) %>% 
-  arrange(targety_first) %>% 
-  mutate(rank = 1:nrow(.)) %>% 
-  filter(rank > 6) %>% 
-  mutate_at(vars(contains("wb")),funs(targety_first - .)) %>% 
-  mutate_at(vars(contains("variable")),funs(targety_first - .)) %>% 
-  ggplot(aes(variable4, wb4)) +
-  geom_point(size = 4) +
-  theme_minimal() +
-  geom_abline(intercept = 0, col = "red", size = 1.5) +
-  xlim(-6,10) +
-  ylim(-6,10) +
-  xlab("WEO Forecast error") +
-  ylab("GEP Forecast error") +
-  theme(axis.text = element_text(size = 18),
-        axis.title = element_text(size = 21))
+ggsave("../IEO_forecasts_material/output/figures/comparison/WB/comparison_recessions.pdf")
+  
+    
 
 
-# Relative table:
+
 
 
 final_sr %>% 
@@ -192,7 +116,9 @@ final_sr %>%
   group_by(recession) %>% 
   summarise(wb2 = round(median(wb2, na.rm = T),2), imf2 = round(median(variable2, na.rm = T),2),
             wb4 = round(median(wb4, na.rm = T),2),imf4 = round(median(variable4, na.rm = T),2)) %>%
-  setNames(c("Recession","Current-year (WB)","Current-year (IMF)","Year-ahead (WB)","Year-ahead (IMF)")) %>% 
+  setNames(c("Recession","Current-year (WB)","Current-year (IMF)","Year-ahead (WB)","Year-ahead (IMF)")) %>%
+  mutate(Recession = case_when(Recession == 1 ~ "Recession",
+                               T ~ "Non-recession")) %>% 
   stargazer(rownames = F,
             summary = F,
             out = "../IEO_forecasts_material/output/tables/comparison/WB/recession_forecast_error.tex")
