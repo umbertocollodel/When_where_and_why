a189ecfd1844d3f94652c2fde6c1374db60fb857
Umberto Collodel <umberto.collodel@gmail.com>
2020-07-22 11:39:54 +0200

    finished standard cleaning world bank data

--- 1.7 preparation_world_bank.R
+++ 1.7 preparation_world_bank.R
@@ -1,8 +1,51 @@
 ##### Script to prepare World Bank forecast data ----
 
-read_xlsx("../IEO_forecasts_material/raw_data/world bank/GEP_forecast_vintages_06232020.xlsx",
+original_wb <- read_xlsx("../IEO_forecasts_material/raw_data/world bank/GEP_forecast_vintages_06232020.xlsx",
           sheet = "Table_long") %>% 
   select(1:4) %>% 
-  setNames(c("date_publication","country","year_forecasted","wb")) %>% 
-  mutate(wb = case_when(wb == "..." ~ NA,
-                        T ~ wb))
+  setNames(c("date_publication","country","year_forecasted","wb")) %>%
+  mutate(wb = as.numeric(wb)) %>% 
+  mutate(year_publication = str_extract(date_publication,"\\d{4}")) %>% 
+  group_by(country) %>% 
+  filter(year_publication == year_forecasted | as.numeric(year_publication) == as.numeric(year_forecasted) -1)
+
+forecasts <- original_wb %>% 
+  split(.$year_forecasted) %>% 
+  map(~ .x %>% select(-year_publication)) %>% 
+  map(~ .x %>% spread(date_publication, wb)) %>% 
+  map(~ .x %>% rename_at(vars(starts_with("January")), ~ paste0(.,"jan"))) %>% 
+  map(~ .x %>% rename_at(vars(starts_with("January")), ~ str_remove(.,"^January"))) %>% 
+  map(~ .x %>% rename_at(vars(starts_with("June")), ~ paste0(.,"jun"))) %>% 
+  map(~ .x %>% rename_at(vars(starts_with("June")), funs(str_remove(.,"^June"))))
+
+
+# Discard years for which no actual value:
+
+forecasts <- forecasts %>% 
+  discard(~ unique(.x$year_forecasted) > 2019)
+
+
+# Order names of the columns:
+
+for(i in 1:length(forecasts)){
+  forecasts[[i]] <- forecasts[[i]][,order(names(forecasts[[i]]))]
+}
+
+
+# Naming similar to Zidong and bind together:
+
+
+final_forecasts <-  forecasts %>% 
+  map(~ if(length(names(.x)) == 6){
+    .x %>% setNames(c(rev(paste0("variable",seq(4:1))),"country","year"))
+  } else if(length(names(.x)) == 4){
+    .x %>% setNames(c(rev(paste0("variable",seq(2:1))),"country","year"))
+    })  %>%  
+  bind_rows() %>% 
+  mutate(country_code = countrycode(country,"country.name","imf")) %>%
+  select(country_code, country, year, variable1, variable2, variable3, variable4) %>% 
+  arrange(country,year)
+
+
+
+  
