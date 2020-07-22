30d7897167198ec41973b69df12916a4e5cef23e
Umberto Collodel <umberto.collodel@gmail.com>
2020-07-22 17:40:03 +0200

    finished scraping ec site

--- 1.8 prepare_european_commission.R
+++ 1.8 prepare_european_commission.R
@@ -1,2 +1,26 @@
-# Script to prepare European Commission data ----
+# Script to prepare European Commission data 
+
+# Scraping EC website and download the files: -----
+
+urls <- read_html("https://ec.europa.eu/info/business-economy-euro/indicators-statistics/economic-databases/macro-economic-database-ameco/ameco-archive_en") %>%
+  html_nodes("a") %>%
+  html_attr("href") %>% 
+  str_subset("ameco_.*\\.zip")
+
+
+names <- urls %>% 
+  str_extract("ameco_.*\\.zip")
+
+
+urls %>% 
+  walk2(names, ~ tryCatch(
+                  expression = {
+                    download.file(.x, paste0("../IEO_forecasts_material/raw_data/european commision/",.y))
+                    cat(crayon::green(paste0(.y," succesfully downloaded.\n")))
+                    },
+                  error = function(e){
+                   cat(crayon::red(paste0("Could not download file: ",.y,"\n")))
+                 }
+  ))
+
 
