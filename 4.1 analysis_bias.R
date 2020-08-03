d94f006f0fab1b6fa1f4ecdc39cbf162f738b6d4
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-03 17:13:35 +0200

    corrected assignment

--- 4.1 analysis_bias.R
+++ 4.1 analysis_bias.R
@@ -2,7 +2,7 @@ list_regressions=c(variable7 ~ 1, variable8 ~ 1, variable9 ~ 1,
                    variable10 ~ 1, variable11 ~ 1, variable12 ~ 1)
 
 
-gdp_medium %>% 
+regressions <- gdp_medium %>% 
   mutate_at(vars(matches("variable")), funs(targety_first - .)) %>% 
   split(.$country) %>% 
   map( ~ map(list_regressions, function(x){
@@ -13,7 +13,7 @@ gdp_medium %>%
 
 
 
-df_bias <- a %>% 
+df_bias <- regressions %>% 
 modify_depth(2, ~ tryCatch(summary(.x), error = function(e){
   cat(crayon::red("Could not run regression. Check dataframe \n"))
 })) %>% 
