605e39c38f43f688a0b8157d3f394ef50d7c635b
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-05 12:47:16 +0200

    further steps but getting tiread, leave for further

--- 4.2 analysis_efficiency.R
+++ 4.2 analysis_efficiency.R
@@ -75,9 +75,8 @@ footnote=c("The table shows results from regressions of year(t + h) errors in th
 
 
 
-# Merge and regress by country: ----
+# Number of countries for which ineffiency: ----
 
-# Merge and regress by group:
 
 
 list_regressions=c(variable7 ~ centre7, variable8 ~ centre8, variable9 ~ centre9,
@@ -86,7 +85,7 @@ list_regressions=c(variable7 ~ centre7, variable8 ~ centre8, variable9 ~ centre9
 regressions <- centre_countries %>% 
   map(~ merge(fe,.x, by=c("year"))) %>%
   map(~ as.tibble(.x)) %>% 
-  map(~ split(.x, .x$country)) %>% 
+  map(~ split(.x, .x$country_code)) %>% 
   modify_depth(2, ~ map(list_regressions, function(x){
     tryCatch(lm(x, .x), error = function(e){
       cat(crayon::red("Could not run the regression. Check data\n"))
@@ -100,6 +99,18 @@ table_medium_efficiency <- regressions %>%
   modify_depth(3, ~ .x[["coefficients"]]) %>% 
   modify_depth(3, ~ .x %>% as_tibble() %>%  slice(2)) %>% 
   modify_depth(2, ~ .x %>% bind_rows(.id = "horizon")) %>% 
-  map(~ .x %>% bind_rows(.id = "country"))
+  map(~ .x %>% bind_rows(.id = "country_code")) %>%
+  map(~ .x %>% mutate(country = countrycode(country_code,"imf","country.name"))) %>% 
+  map(~ .x %>% mutate(horizon = case_when(horizon == 1 ~ "H=3,Fall",
+                                          horizon == 2 ~ "H=3,Spring",
+                                          horizon == 3 ~ "H=4,Fall",
+                                          horizon == 4 ~ "H=4,Spring",
+                                          horizon == 5 ~ "H=5,Fall",
+                                          T ~ "H=5,S"))) %>% 
+  map(~ .x %>% separate(horizon, c("horizon","issue"), sep = ",")) %>%
+  map(~ .x %>% filter(`t value` >= 1.96) %>% select(country_code, country, horizon, issue, Estimate)) %>% 
+  map(~ .x %>% merge(geo_group)) 
+
+# TO COMPLETE!
   
 
