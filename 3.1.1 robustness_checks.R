92ed2da800573e8e244407a412f87470153e0432
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-07 16:21:50 +0200

    are jan and july updates for all countries? bad code

---
+++ 3.1.1 robustness_checks.R
@@ -0,0 +1,41 @@
+# First robustness check: are year-ahead WEO Fall forecasts different from WEO current-year January forecasts? -----
+
+# Fall-spring data
+
+load("../IEO_forecasts_material/intermediate_data/rgdp_cleaned.RData")
+rgdp_fall_spring <- x
+
+load("../IEO_forecasts_material/intermediate_data/rgdp_jan_update.RData")
+rgdp_jan <- x %>% select(-variable2)
+
+rgdp_fall_spring_clean1 <- rgdp_fall_spring %>% 
+  select(country_code, year, variable3)
+
+
+merge(rgdp_jan,rgdp_fall_spring_clean1) %>% 
+  as_tibble() %>% 
+  mutate(no_change = case_when(variable1 != variable3 ~ 0,
+                               T ~ 1)) %>%
+  summarise(mean_no_change = mean(as.numeric(no_change), na.rm = T))
+
+
+# Second robustness check: are current-year WEO Spring forecasts different from WEO current-year July forecasts? -----
+
+
+update <- readRDS("../IEO_forecasts_material/intermediate_data/rgdp_update_cleaned.RDS")
+
+update_july_clean <- update %>% 
+  select(country_code, year, variable1)
+
+load("../IEO_forecasts_material/intermediate_data/rgdp_cleaned.RData")
+rgdp_fall_spring <- x
+
+rgdp_fall_spring_clean2 <- rgdp_fall_spring %>% 
+  select(country_code, year, variable2)
+
+merge(update_july_clean,rgdp_fall_spring_clean2) %>% 
+  as_tibble() %>% 
+  mutate(no_change = case_when(variable1 != variable2 ~ 0,
+                               T ~ 1)) %>%
+  summarise(mean_no_change = mean(as.numeric(no_change), na.rm = T))
+
