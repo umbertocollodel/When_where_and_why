4caa4f61aa6ade0fd4c241332549dbf4c7e122e5
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-11 16:05:53 +0200

    finalized correction of dataframe

--- 1.8 prepare_mona_macro.R
+++ 1.8 prepare_mona_macro.R
@@ -2,8 +2,11 @@
 
 
 # Clean MONA with macro-variables 2002-2020 ----
-# Note: for the moment we focus only on board approval forecasts
+# Note1: for the moment we focus only on board approval forecasts
 # in the future work also on correction of forecasts with reviews.
+# Note2: variable2 is the next year year-ahead forecast! Correction in last paragraph, but
+# dataframe still useful for summary stats.
+
 
 mona_macro_raw <- read_excel("../IEO_forecasts_material/raw_data/mona/mona_2002-2020_macro.xlsx") %>%
   filter(`Review Type` == "R0") %>% 
@@ -17,15 +20,8 @@ mona_macro_raw <- read_excel("../IEO_forecasts_material/raw_data/mona/mona_2002-
   mutate(year_ahead = year +1) %>% 
   mutate(country = str_to_sentence(tolower(country))) %>% 
   mutate(country_code = countrycode(country,"country.name","imf")) %>% 
-  mutate(date = as.Date(date)) 
-
-
-mona_macro <- list(current = mona_macro_raw %>% select(program_id, country_code, country, program_type, date, year, variable1),
-     year_ahead = mona_macro_raw %>% select(program_id, country_code, country, program_type, date, year_ahead, variable2)) %>% 
-  map(~ .x %>% rename_at(vars(contains("year")),funs(str_remove(.,"_.*")))) %>% 
-  reduce(merge, all = T) %>% 
-  as_tibble() 
-
+  mutate(date = as.Date(date)) %>% 
+  select(-country)
 
 
 
@@ -40,29 +36,40 @@ mona_amount <- read_excel("../IEO_forecasts_material/raw_data/mona/mona_amounts.
   setNames(c("country_code","year","date","precautionary","exceptional_access","original_duration","amount_drawn","amount_percent_quota")) %>% 
   mutate(country = countrycode(country_code,"imf","country.name")) %>%
   mutate_at(vars(contains("amount")),funs(as.numeric(.))) %>%
-  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30")) %>% 
+  mutate_at(vars(precautionary:exceptional_access),funs(case_when(. == "n.a." ~ NA_character_,
+                                                                  . == ".." ~ NA_character_,
+                                                                  T ~ .))) %>%
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30")) %>%
   filter(year >= 2002) %>% 
   select(country_code, country, everything()) 
 
-# Combine the two and export: ----
+# Combine and export: ----
 # Note: check why duplicates are formed in first place.
 
 
-final_mona <- merge(mona_macro, mona_amount, all.x = T) %>% 
+final_mona_raw <- merge(mona_macro_raw, mona_amount, by=c("country_code","year","date")) %>% 
   as_tibble() %>%
   arrange(country) %>%
-  group_by(country) %>% 
-  mutate_at(vars(precautionary:amount_percent_quota), 
-            funs(case_when(dplyr::lag(program_id,1) == program_id ~ dplyr::lag(.,1),
-                 T ~ .))) %>% 
   distinct(country,year,program_id, .keep_all = T) %>% 
-  mutate_at(vars(precautionary:exceptional_access),funs(case_when(. == "n.a." ~ NA_character_,
-                                                                  . == ".." ~ NA_character_,
-                                                                  T ~ .))) %>% 
   select(country_code, country, everything())
 
-# Export:
 
+
+# Correction for year-ahead forecasts ----
+
+final_mona <- list(current = final_mona_raw %>% select(-year_ahead,-variable2),
+                   year_ahead = final_mona_raw %>% select(-year,-variable1)) %>% 
+  map(~ .x %>% rename_at(vars(contains("year")),funs(str_remove(.,"_.*")))) %>% 
+  reduce(merge, all = T) %>% 
+  as_tibble() %>% 
+  arrange(country)
+
+# Export: ----
+
+final_mona_raw %>% 
+  select(-year_ahead) %>% 
+  saveRDS("../IEO_forecasts_material/intermediate_data/mona/mona_summary_stat.RDS")
 
 final_mona %>% 
-saveRDS("../IEO_forecasts_material/intermediate_data/mona/mona_macro_clean.RDS")
+  saveRDS("../IEO_forecasts_material/intermediate_data/mona/mona_macro_clean.RDS")
+
