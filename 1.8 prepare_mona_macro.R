60dcab15141dcd5929de0b674d950a4e4cd3798c
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-11 15:25:34 +0200

    corrected mistake:variable2 refers to next year, not current one

--- 1.8 prepare_mona_macro.R
+++ 1.8 prepare_mona_macro.R
@@ -5,19 +5,28 @@
 # Note: for the moment we focus only on board approval forecasts
 # in the future work also on correction of forecasts with reviews.
 
-mona_macro <- read_excel("../IEO_forecasts_material/raw_data/mona/mona_2002-2020_macro.xlsx") %>%
+mona_macro_raw <- read_excel("../IEO_forecasts_material/raw_data/mona/mona_2002-2020_macro.xlsx") %>%
   filter(`Review Type` == "R0") %>% 
   filter(Description == "Gross domestic product, constant prices") %>% 
   select(`Arrangement Number`,`Country Name`,`Arrangement Type`,`Approval Date`,
          `Approval Year`,`T-1`,`T`,`T+1`) %>% 
   mutate(`T+1` = ((`T+1` - `T`)/`T`)*100,
          `T` = ((`T` - `T-1`)/`T-1`)*100) %>% 
-  select(-`T-1`) %>% 
+  select(-`T-1`) %>%
   setNames(c("program_id","country","program_type","date","year","variable1","variable2")) %>%
+  mutate(year_ahead = year +1) %>% 
   mutate(country = str_to_sentence(tolower(country))) %>% 
   mutate(country_code = countrycode(country,"country.name","imf")) %>% 
-  mutate(date = as.Date(date)) %>% 
-  select(country_code, program_id,date, year, program_type, variable1, variable2) 
+  mutate(date = as.Date(date)) 
+
+
+mona_macro <- list(current = mona_macro_raw %>% select(program_id, country_code, country, program_type, date, year, variable1),
+     year_ahead = mona_macro_raw %>% select(program_id, country_code, country, program_type, date, year_ahead, variable2)) %>% 
+  map(~ .x %>% rename_at(vars(contains("year")),funs(str_remove(.,"_.*")))) %>% 
+  reduce(merge, all = T) %>% 
+  as_tibble() 
+
+
 
 
 # Clean sheet with dummy for exceptional access: ----
@@ -36,13 +45,22 @@ mona_amount <- read_excel("../IEO_forecasts_material/raw_data/mona/mona_amounts.
   select(country_code, country, everything()) 
 
 # Combine the two and export: ----
-# why the number of programs decreases?
+# Note: check why duplicates are formed in first place.
 
 
-final_mona <- merge(mona_macro, mona_amount) %>% 
+final_mona <- merge(mona_macro, mona_amount, all.x = T) %>% 
   as_tibble() %>%
-  arrange(country) %>% 
+  arrange(country) %>%
+  group_by(country) %>% 
+  mutate_at(vars(precautionary:amount_percent_quota), 
+            funs(case_when(dplyr::lag(program_id,1) == program_id ~ dplyr::lag(.,1),
+                 T ~ .))) %>% 
+  distinct(country,year,program_id, .keep_all = T) %>% 
+  mutate_at(vars(precautionary:exceptional_access),funs(case_when(. == "n.a." ~ NA_character_,
+                                                                  T ~ .))) %>% 
   select(country_code, country, everything())
+
+# Export:
 
 
 final_mona %>% 
