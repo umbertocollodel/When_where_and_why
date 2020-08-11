f3e477c54795505ca7f6acae1929dbf181e9a05c
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-11 10:20:50 +0200

    corrected bug of second word country lower case in one df and upper in other

--- 1.8 prepare_mona_macro.R
+++ 1.8 prepare_mona_macro.R
@@ -17,5 +17,30 @@ mona_macro <- read_excel("../IEO_forecasts_material/raw_data/mona/mona_2002-2020
   mutate(country = str_to_sentence(tolower(country))) %>% 
   mutate(country_code = countrycode(country,"country.name","imf")) %>% 
   mutate(date = as.Date(date)) %>% 
-  select(country_code, country, program_id,date, year, program_type, variable1, variable2)
+  select(country_code, program_id,date, year, program_type, variable1, variable2) 
+
+
+# Clean sheet with dummy for exceptional access: ----
+# Note: origin for date format changes according to machine used.
+
+mona_amount <- read_excel("../IEO_forecasts_material/raw_data/mona/mona_amounts.xlsx") %>% 
+  select(-c(1, 7, 18, 20, 22, 24, 26, 28, 30, 35, 37, 40)) %>% 
+  slice(-1) %>% 
+  row_to_names(1) %>%
+  select(`IFS Code`,Year,`Date of Arrangement`,`Precautionary at program approval`,`Exceptional Access`,`Original Duration (Months)`,`Total amount disbursed`,`Total Approved Amount (percent of current quota)`) %>%
+  setNames(c("country_code","year","date","precautionary","exceptional_access","original_duration","amount_drawn","amount_percent_quota")) %>% 
+  mutate(country = countrycode(country_code,"imf","country.name")) %>%
+  mutate_at(vars(contains("amount")),funs(as.numeric(.))) %>%
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30")) %>% 
+  filter(year >= 2002) %>% 
+  select(country_code, country, everything()) 
+
+# Combine the two and export: ----
+# why the number of programs decreases?
+
+
+final_mona <- merge(mona_macro, mona_amount) %>% 
+  as_tibble() %>%
+  arrange(country) %>% 
+  select(country_code, country, everything())
 
