6c35f478fdeff06c6942817999619f96985c8318
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-18 10:44:12 +0200

    added package

--- 5. mona_source.R
+++ 5. mona_source.R
@@ -9,7 +9,7 @@ remove(list = ls())
 
 packages <- c("openxlsx","purrr","tidyverse", "readxl","countrycode","roxygen2",
               "stargazer","hydroGOF","rio","forecast","janitor", "rvest",
-              "pdftools") 
+              "pdftools","DescTools") 
 
 
 lapply(packages, function(x){
