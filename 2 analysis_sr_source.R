f22839830ec832a83d8439c6aa971f59acf135d7
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-19 16:53:24 +0200

    fixed all read of RDATA into RDS in source file

--- 2 analysis_sr_source.R
+++ 2 analysis_sr_source.R
@@ -1,4 +1,6 @@
-# Prepare the dataset: ----
+# Prepare the datasets: ----
+
+rm(list = ls())
 
 # Load economic and geographical groups:
 
@@ -6,17 +8,32 @@ country_group <- read_xlsx("../IEO_forecasts_material/raw_data/country_group.xls
   rename(country_code = ifscode) %>% 
   select(country_code, adv, eme, lidc)
 
-load("../IEO_forecasts_material/intermediate_data/country_group_geography_clean.RData")
+geo_group <- readRDS("../IEO_forecasts_material/intermediate_data/country_group_geography_clean.RDS")
 
+# Load dataset:
 
-final_sr <- final %>% 
+paths=c("../IEO_forecasts_material/intermediate_data/rgdp_cleaned.RDS",
+        "../IEO_forecasts_material/intermediate_data/inflation_cleaned.RDS")
+
+names=c("growth",
+        "inflation")
+
+final_sr <- paths %>%
+  map(~ readRDS(.x)) %>% 
   map(~ .x %>% select(country_code,country,year,targety_first,targety_last,matches("variable[1-4]$"))) %>% 
   map(~ .x %>% merge(country_group, by = c("country_code"))) %>%
-  map(~ .x %>% merge(x, by = c("country_code"),all.x = T)) %>% 
-  map(~ .x %>% mutate(period = case_when(year <= "2001" ~ "1990:2001",
-                                         year > "2001" & year <= "2011" ~ "2002:2011",
-                                         TRUE ~ "2012:2018"))) %>% 
+  map(~ .x %>% merge(geo_group, by = c("country_code"),all.x = T)) %>% 
   map(~ .x %>% as.tibble())
+
+
+names(final_sr) <- names
+
+# Years of recession:
+
+years_recession <- final_sr[["growth"]] %>% 
+  mutate(recession = case_when(targety_first <= 0 ~ 1,
+                                      TRUE ~ 0)) %>% 
+  select(country_code, year, recession)
 
 
 # Source and produce output: ----
