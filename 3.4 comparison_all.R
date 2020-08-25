a952f912a5ad9c0cfd3a7cb2e3e9afdb28f3b9c8
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-25 10:57:21 +0200

    cleaned code

--- 3.4 comparison_all.R
+++ 3.4 comparison_all.R
@@ -1,46 +1,28 @@
-# Inability to predict recessions
+############# Script to summarise previous tables on inability to predict recessions
+############# by major institutions:
 
+# Inability to predict recessions:
 
-a <- comparison_wb %>% 
-  mutate(recession = case_when(targety_first <= 0 ~ 1,
-                               T ~ 0)) %>% 
-  mutate_at(vars(contains("wb")),funs(targety_first - .)) %>% 
-  select(country_code,year,wb3, recession) 
-
-b <- comparison_ec %>% 
-  mutate_at(vars(contains("ec")),funs(targety_first - .)) %>% 
-  mutate(recession = case_when(targety_first <= 0 ~ 1,
-                               T ~ 0)) %>% 
-  select(country_code,year,ec3, recession) 
-
-
-c <- comparison_consensus %>% 
-  mutate(recession = case_when(targety_first <= 0 ~ 1,
-                               T ~ 0)) %>% 
-  mutate_at(vars(contains("consensus")),funs(targety_first - .)) %>% 
-  select(country_code,year,consensus3, recession) %>% 
-  rename_at(vars(contains("consensus")),funs(str_replace(.,"consensus","cn")))
-
-e <- d[[3]] %>%
-  rename_at(vars(contains("consensus")),funs(str_replace(.,"consensus","cb"))) %>% 
-  mutate(country_code = countrycode(country,"country.name","imf")) %>% 
-  select(country_code,year,cb3, recession) 
-  
-  
-
-list(a,b,c,e) %>% 
-  reduce(merge, all=T) %>%
-  as_tibble() %>% 
-  gather("institution","fe",wb3:cb3) %>%
-  mutate(recession = case_when(recession == 0 ~ "Non-recession",
-                               T ~ "Recession")) %>% 
-  mutate(institution = case_when(institution == "wb3" ~ "World Bank",
-                                 institution == "ec3" ~ "European Commission",
-                                 institution == "cn3" ~ "Consensus (Mean)",
-                                 T ~ "Consensus (Best)")) %>% 
+list(comparison_wb,comparison_ec,comparison_consensus) %>% 
+  map(~ .x %>% mutate_at(vars(matches("wb\\d|ec\\d|consensus\\d")),funs(targety_first - .))) %>% 
+  map(~ .x %>% mutate(recession = case_when(targety_first <= 0 ~ "Recession",
+                                            T ~ "Non-recession"))) %>% 
+  map(~ .x %>% select(country_code,year,matches("wb\\d|ec\\d|consensus\\d"),recession)) %>% 
+  map(~ .x %>% rename_at(vars(matches("consensus")),funs(str_replace(.,"consensus","cn")))) %>% 
+  reduce(merge, all=T) %>% 
+  gather("institution","fe",wb1:ncol(.)) %>%
+  separate(institution, c("institution","horizon"), sep = 2) %>% 
+  mutate(institution = case_when(institution == "wb" ~ "World Bank",
+                                 institution == "ec" ~ "European Commission",
+                                 institution == "cn" ~ "Consensus (Mean)")) %>% 
+  mutate(horizon = case_when(horizon == 1 ~ "H=0,F",
+                                 horizon == 2 ~ "H=0,S",
+                                 horizon == 3 ~ "H=1,F",
+                                 T ~ "H=1,S")) %>% 
   ggplot(aes(x=fe,y=institution, fill = recession)) +
   geom_density_ridges(col="white",alpha = 0.4, scale=0.95) +
   theme_minimal() +
+  facet_wrap(~ horizon) +
   scale_fill_manual(values = c("#0000ff","#ff0000")) +
   ylab("") +
   xlab("Real Growth Forecast Error (%)") +
@@ -50,6 +32,8 @@ list(a,b,c,e) %>%
         axis.title = element_text(size = 21),
         legend.title = element_text(size = 18),
         legend.text = element_text(size = 16))
+
+  
 
 
 ggsave("../IEO_forecasts_material/output/figures/comparison/inability_recessions.pdf")
