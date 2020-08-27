febed90aa5e05c0f8dc25a6359ca68f83caf1450
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-27 09:59:16 +0200

    added imf to graph and corrected mistake with consensus: was keeping all forecasters

--- 3.4 comparison_all.R
+++ 3.4 comparison_all.R
@@ -3,17 +3,29 @@
 
 # Inability to predict recessions:
 
-list(comparison_wb,comparison_ec,comparison_consensus) %>% 
-  map(~ .x %>% mutate_at(vars(matches("wb\\d|ec\\d|consensus\\d")),funs(targety_first - .))) %>% 
+inability_df <- list(final_sr$growth,
+                     comparison_wb,
+                     comparison_ec,
+                     comparison_consensus %>% filter(forecaster == "Consensus (Mean)")) %>% 
+  map(~ .x %>% mutate_at(vars(matches("variable\\d|wb\\d|ec\\d|consensus\\d")),funs(targety_first - .))) %>% 
   map(~ .x %>% mutate(recession = case_when(targety_first <= 0 ~ "Recession",
-                                            T ~ "Non-recession"))) %>% 
-  map(~ .x %>% select(country_code,year,matches("wb\\d|ec\\d|consensus\\d"),recession)) %>% 
+                                            T ~ "Non-recession"))) 
+
+
+inability_df[[1]] <- inability_df[[1]] %>% 
+  rename_at(vars(matches("variable")),funs(str_replace(.,"variable","if"))) 
+
+
+
+inability_df %>% 
+  map(~ .x %>% select(country_code,year,matches("if\\d|wb\\d|ec\\d|consensus\\d"),recession)) %>% 
   map(~ .x %>% rename_at(vars(matches("consensus")),funs(str_replace(.,"consensus","cn")))) %>% 
   reduce(merge, all=T) %>% 
-  gather("institution","fe",wb1:ncol(.)) %>%
+  gather("institution","fe",if1:ncol(.)) %>%
   separate(institution, c("institution","horizon"), sep = 2) %>% 
   mutate(institution = case_when(institution == "wb" ~ "World Bank",
                                  institution == "ec" ~ "European Commission",
+                                 institution == "if" ~ "IMF",
                                  institution == "cn" ~ "Consensus (Mean)")) %>% 
   mutate(horizon = case_when(horizon == 1 ~ "H=0,F",
                                  horizon == 2 ~ "H=0,S",
