901a63e476cdef37fa4da8465a4cc57eb18117ac
Umberto Collodel <umberto.collodel@gmail.com>
2020-08-27 09:52:00 +0200

    added ridges package

--- 1. prepare_source.R
+++ 1. prepare_source.R
@@ -9,7 +9,7 @@ remove(list = ls())
 
 packages <- c("openxlsx","purrr","tidyverse", "readxl","countrycode","roxygen2",
               "stargazer","hydroGOF","rio","forecast","janitor", "rvest",
-              "pdftools","lubridate") 
+              "pdftools","lubridate","ggridges") 
 
 
 lapply(packages, function(x){
