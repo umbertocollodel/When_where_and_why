36000ec245cdcf2ec8cee72010fd8f650bea2f69
Umberto Collodel <umberto.collodel@gmail.com>
2020-12-01 13:32:40 +0100

    run for consensus and put common footnote

--- 2.3 analysis_sr_bias_others.R
+++ 2.3 analysis_sr_bias_others.R
@@ -1,6 +1,6 @@
 ################### Script to analyze bias in publications other than WEO
 # Note: might take a while to run because of heavy use of bootstrap. To lower time, decrease the number
-# of replications
+# of replications.
 
 
 # Set common parameters: ----
@@ -16,15 +16,25 @@ seed=253
 name_issues=c("Summer","Winter")
 
 
-analyse_sr_bias_others(comparison_wb, seed, "wb", replication, c("Summer","Winter"))
+analyse_sr_bias_others(comparison_wb, seed, "wb", replication, name_issues)
 
 
-footnote=c("The figure shows the share of countries for each forecast horizon and issue of the Global Economic
-           Prospects (Summer or Winter) with a 5% statistically signicant negative or positive bias. Test of statistical
-           significance is run individually with country-by-country regressions where the standard errors are obtained
+# Run for Consensus -----
+
+name_issues=c("Fall","Spring")
+
+
+analyse_sr_bias_others(comparison_consensus %>% filter(forecaster == "Consensus (Mean)"),seed,"consensus",replication, name_issues)
+
+
+
+# Common footnote: -----
+
+footnote=c("The figure shows the share of countries for each forecast horizon and issue of Consensus survey 
+           (Fall or Spring) and World Bank Global Economic Prospects (Summer or Winter) with a 5% 
+           statistically significant negative or positive bias. Test of statistical significance is run 
+           individually with country-by-country regressions where the standard errors are obtained
            from a 1,000 replications bootstrap.") %>% 
-  cat(file = "../IEO_forecasts_material/output/figures/short-run forecasts/bias_others/wb_foonote.tex")
-
-
+  cat(file = "../IEO_forecasts_material/output/figures/short-run forecasts/bias_others/bias_others_foonote.tex")
 
   
