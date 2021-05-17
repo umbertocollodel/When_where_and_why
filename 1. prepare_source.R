a0bb13cb280a35b9c9cd44e2407e397f522ede25
Umberto Collodel <umberto.collodel@gmail.com>
2021-05-17 10:52:38 +0100

    added package for aes financial crises figure

--- 1. prepare_source.R
+++ 1. prepare_source.R
@@ -9,7 +9,7 @@ remove(list = ls())
 
 packages <- c("openxlsx","purrr","tidyverse", "readxl","countrycode","roxygen2",
               "stargazer","hydroGOF","rio","forecast","janitor", "rvest",
-              "pdftools","lubridate","ggridges","modelr") 
+              "pdftools","lubridate","ggridges","modelr", "scales") 
 
 
 lapply(packages, function(x){
